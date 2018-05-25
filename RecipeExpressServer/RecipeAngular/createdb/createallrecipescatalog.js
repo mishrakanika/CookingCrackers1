4620624ccb9e0279d3003abba08298b7ffa0d189
divyanshiy12 <yadavd@seattleu.edu>
2018-05-25 16:18:36 -0700

    new create db for catalog and recipe 2 files

--- RecipeExpressServer/RecipeAngular/createdb/createallrecipescatalog.js
+++ RecipeExpressServer/RecipeAngular/createdb/createallrecipescatalog.js
@@ -1,130 +1,131 @@
 db = db.getSiblingDB('Recipesdb')
-db.createCollection('mealtype')  //earlier it was lists
-mealtypeCollection = db.getCollection("mealtype") //earlier it was lists & mealtypeCollection was listsCollection
-mealtypeCollection.remove({})   //mealtypeCollection was listsCollection
-mealtypeCollection.insert(   //mealtypeCollection was listsCollection
+db.createCollection('recipecatalog')  //earlier it was lists
+recipecatalogCollection = db.getCollection("recipecatalog") //earlier it was lists & recipeCatalog was listsCollection
+recipecatalogCollection.remove({})   //recipeCatalog was listsCollection
+recipecatalogCollection.insert(   //recipeCatalog was listsCollection
 {
-      name: "Breakfast",
-      mealtId: 1,
+      name: "Meal Type",
+      rcId: 1,
       
 }
 )
-mealtypeCollection.insert(
+recipecatalogCollection.insert(
 {
-      name: "Lunch",
-      mealtId: 2,
+      name: "Cuisine Type",
+      rcId: 2,
 }
 )
-mealtypeCollection.insert(
+recipecatalogCollection.insert(
 {
-      name: "Dinner",
-      lmealtId: 3,
+      name: "Meal Preference",
+      rcId: 3,
 
 })
-mealtypeCollection.insert(
-    {
-          name: "Midnight Snacks",
-          mealtId: 4,
-    
-    }
-)
-mealtypeCollection.insert(
-    {
-          name: "Desserts",
-          mealtId: 5,
-    
-    }
-)
-db = db.getSiblingDB('Recipesdb')
-db.createCollection('cuisinetype')  //earlier it was lists
-cuisinetypeCollection = db.getCollection("cuisinetype") //earlier it was lists & cuisinetypeCollection was listsCollection
-cuisinetypeCollection.remove({})  //cuisinetypeCollection was listsCollection
-cuisinetypeCollection.insert(  //cuisinetypeCollection was listsCollection
-{
-      name: "Indian",
-      cuisinetId: 1,
-      
-}
-)
-cuisinetypeCollection.insert(
-{
-      name: "Mexican",
-      cuisinetId: 2,
-}
-)
-cuisinetypeCollection.insert(
-{
-      name: "Italian",
-      cuisinetId: 3,
 
-})
-cuisinetypeCollection.insert(
-    {
-          name: "Chinese",
-          cuisinetId: 4,
-    
-    }
-)
-cuisinetypeCollection.insert(
-    {
-          name: "Mediterranean",
-          cuisinetId: 5,
-    
-    }
-)
-cuisinetypeCollection.insert(
-      {
-            name: "American",
-            cuisinetId: 6,
-      
-      }
-  )
-db = db.getSiblingDB('Recipesdb')
-db.createCollection('mealpreference')  //earlier it was lists
-mealpreferenceCollection = db.getCollection("mealpreference") //earlier it was lists & mealpreferenceCollection was listsCollection
-mealpreferenceCollection.remove({})  //mealpreferenceCollection was listsCollection
-mealpreferenceCollection.insert(  //mealpreferenceCollection was listsCollection
-{
-      name: "Diabetic",
-      mealpId: 1,
-      
-}
-)
-mealpreferenceCollection.insert(
-{
-      name: "Vegetarian",
-      mealpId: 2,
-}
-)
-mealpreferenceCollection.insert(
-{
-      name: "NonVegetarian",
-      mealpId: 3,
 
+
+db.createCollection('recipecatalogtype')  //earlier it was lists
+recipecatalogtypeCollection = db.getCollection("recipecatalogtype") //earlier it was lists & cuisinetypeCollection was listsCollection
+recipecatalogtypeCollection.remove({})  //cuisinetypeCollection was listsCollection
+recipecatalogtypeCollection.insert(  //cuisinetypeCollection was listsCollection
+{
+            rcId:1,
+            recipecatalogtype:[
+                  {
+                        name: "Breakfast",
+                        rctypeId: 1
+                  },
+                  {
+                        name: "Lunch",
+                        rctypeId: 2
+                  },
+                  {
+                        name: "Dinner",
+                        rctypeId: 3
+                  },
+                  {
+                        name: "Midnight Snacks",
+                        rctypeId: 4
+                  },
+                  {
+                        name: "Desserts",
+                        rctypeId: 5
+                  }
+      
+            ]
+            
 })
-mealpreferenceCollection.insert(
-    {
-          name: "GlutenFree",
-          mealpId: 4,
-    
-    }
+recipecatalogtypeCollection.insert(  
+{
+      rcId:2,
+      recipecatalogtype:[
+            {
+                  name: "Indian",
+                  rctypeId: 1
+            },
+            {
+                  name: "Mexican",
+                  rctypeId: 2
+            },
+            {
+                  name: "Italian",
+                  rctypeId: 3
+            },
+            {
+                  name: "Chinese",
+                  rctypeId: 4
+            },
+            {
+                  name: "Mediterranean",
+                  rctypeId: 5
+            },
+            {
+                  name: "American",
+                  rctypeId: 6     
+            }
+
+      ]
+      
+}
 )
-mealpreferenceCollection.insert(
-    {
-          name: "LowCalorie",
-          mealpId: 5,
-    
-    }
-)
+recipecatalogtypeCollection.insert(  //cuisinetypeCollection was listsCollection
+ {
+                  rcId:3,
+                  recipecatalogtype:[
+                        {
+                              name: "Diabetic",
+                              rctypeId: 1
+                        },
+                        {
+                              name: "Vegetarian",
+                              rctypeId: 2
+                        },
+                        {
+                              name: "Non Vegeratian",
+                              rctypeId: 3
+                        },
+                        {
+                              name: "Gluten Free",
+                              rctypeId: 4
+                        },
+                        {
+                              name: "Low Calorie",
+                              rctypeId: 5
+                        }
+            
+                  ]
+                  
+ })
+
 db.createCollection('Recipesdb')  
 recipesCollection = db.getCollection("recipes") //earlier it was tasks
 recipesCollection.remove({})  //earlier it was tasksCollection
 recipesCollection.insert(  //earlier it was tasksCollection
 {
-	
-	 
-            rname: "Upma",
-            rmethod: "abc",
+	mealtId : 1,
+	recipes : [
+	 {
+		rname: "Upma",
 		rdescription: "1 Place semolina in cast iron pan and cook over medium heat until toasty but not browned, about 2 minutes. Transfer to a bowl.2.Return pan to medium heat and heat ghee or oil until shimmering. Add the mustard seeds and split black gram, stir until they splutter, about 30 seconds. Add the onion, fry until slightly soft, about a minute (take care not to brown). Add the slit chillis, curry leaves and stir until fragrant, about a minute longer. Add the carrots, peas, red chilli powder, and sugar, and stir until fragrant and the mixture is coated with the ghee/oil, about 1 minute.3.Add toasted semolina and stir vigorously until the mixture resembles wet sand—about a minute. Add the salt and gently pour in the water. The semolina will bubble and spurt as it absorbs the water. Reduce heat to low. Allow this mixture to cook for about 2 minutes, stirring constantly. The Upma absorbs water very quickly and tends to dry out too, so turn off the heat when the mixture is slightly runny. Serve immediately.",
 		rrecipeId: 1,
 		rcuisinetype: "Indian",
@@ -136,12 +137,9 @@ recipesCollection.insert(  //earlier it was tasksCollection
             rimage:"https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Roop/Traditional_Rava_Upma.jpg"
   
   
-       },
-       
-      
+	 },
 	 {
-            rname: "French Toast",
-            rmethod: "abc",
+		rname: "French Toast",
 		rdescription: "In a small bowl, combine, cinnamon, nutmeg, and sugar and set aside briefly.In a 10-inch or 12-inch skillet, melt butter over medium heat. Whisk together cinnamon mixture, eggs, milk, and vanilla and pour into a shallow container such as a pie plate. Dip bread in egg mixture. Fry slices until golden brown, then flip to cook the other side. Serve with syrup.",
 		rrecipeId: 2,
 		rcuisinetype: "American",
@@ -153,8 +151,7 @@ recipesCollection.insert(  //earlier it was tasksCollection
             rimage:"https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/q/800/quick-and-easy-french-toast.ashx?vd=20171018T133744Z&hash=7AD28A0C971AAAAF2DEAD18CE67FCC8E36FA9DF2"
        },
        {
-            rname: "Creamy Garlic Penne Pasta",
-            rmethod: "abc",
+		rname: "Creamy Garlic Penne Pasta",
             rdescription: "Melt butter in a medium sauce pan, add garlic. Cook for 1 minute over medium heat. Add flour and cook for an additional minute, stirring constantly. Add milk and broth, stirring constantly. Cook until sauce boils and thickens. Add parsley and parmesan cheese. Add desired salt and pepper. Continue stirring until cheese has melted. Serve immediately. Pour sauce over pasta and serve.",
             rrecipeId: 3,
 		rcuisinetype: "Italian",
@@ -168,8 +165,7 @@ recipesCollection.insert(  //earlier it was tasksCollection
   
 	 },
 	 {
-            rname: "Brownies",
-            rmethod: "abc",
+		rname: "Brownies",
             rdescription: "Preheat the oven to 350°F (175°C) then line a 7x11 inch baking trey with parchment paper and set aside. In a large bowl combine melted butter, oil and both sugars. Add the eggs, vanilla and salt then whisk for about one minute until evenly combined and light in color. Over the same bowl sift in the flour and cocoa powder. Gently fold the dry ingredients into the wet ingredients until JUST combined (do NOT over mix). Fold in half of the chocolate chunks. Pour the batter into the prepared pan, then smooth the top. Generously top with the remaining chocolate chunks. Bake for 35-40 minutes, or until the centre of the brownies no longer jiggles and is JUST set to the touch. Remove from the oven and allow to cool to room temperature before removing from the baking trey and slicing into 16 brownies. Enjoy!",
             rrecipeId: 4,
 		rcuisinetype: "American",
@@ -181,8 +177,7 @@ recipesCollection.insert(  //earlier it was tasksCollection
             rimage:"https://www.twopeasandtheirpod.com/wp-content/uploads/2011/02/Fudgy-brownies.jpg"
 	 },
 	 {
-            rname: "Meatball Nirvana",
-            rmethod: "abc",
+		rname: "Meatball Nirvana",
             rdescription: "Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes. ",
             rrecipeId: 5,
 		rcuisinetype: "American",
@@ -196,8 +191,7 @@ recipesCollection.insert(  //earlier it was tasksCollection
   
 	 },
 	 {
-            rname: "Lime Chicken",
-            rmethod: "abc",
+		rname: "Lime Chicken",
             rdescription: "In a large mixing bowl or large sealable, Ziplock bag, combine the chicken breasts, freshly squeezed lime juice, ground cumin, sea salt, ground black pepper, cayenne, and fresh cilantro. Toss to combine and coat the chicken well. Cover with plastic wrap (or close Ziplock bag tightly) and place in the fridge for at least 30 minutes, preferably 2-3 hours, to allow the marinade to work. Heat a skillet over medium-high heat with the olive oil. Remove the chicken from the fridge and place in the hot oil. Cook the chicken until well-browned on both sides and cooked through, about 4-6 minutes per side, or until the chicken reaches 165 degrees F internal temperature. Allow to rest for 5 minute before slicing and serving.",
             rrecipeId: 6,
 		rcuisinetype: "Chinese",
@@ -209,9 +203,9 @@ recipesCollection.insert(  //earlier it was tasksCollection
             rimage:"https://www.simplyrecipes.com/wp-content/uploads/2013/07/grilled-cilantro-lime-chicken-vertical-a-1200.jpg"
        },
        {
-            rname: "Butter Pecan Fudge",
-            rmethod: "abc",
-            rdescription: "Grease an 8 x 8 inch square pan or line with parchment paper. Add butter, cream, white sugar, brown sugar, and salt to a large, heavy saucepan. Bring to a rolling boil over medium heat, stirring frequently. Cook for 5 minutes, stirring constantly, then remove the pan from the heat. It is important to boil the mixture for the full 5 minutes to get a smooth texture, otherwise the fudge might be crumbly and grainy. Stir in pecans and vanilla extract. Whisk in powdered sugar until smooth. Spread in an even layer in the prepared baking dish, then cool to room temperature or until firm. Remove from the pan and cut into small squares.", rrecipeId: 7,
+		rname: "Butter Pecan Fudge",
+            rdescription: "Grease an 8 x 8 inch square pan or line with parchment paper. Add butter, cream, white sugar, brown sugar, and salt to a large, heavy saucepan. Bring to a rolling boil over medium heat, stirring frequently. Cook for 5 minutes, stirring constantly, then remove the pan from the heat. It is important to boil the mixture for the full 5 minutes to get a smooth texture, otherwise the fudge might be crumbly and grainy. Stir in pecans and vanilla extract. Whisk in powdered sugar until smooth. Spread in an even layer in the prepared baking dish, then cool to room temperature or until firm. Remove from the pan and cut into small squares.",
+            rrecipeId: 7,
 		rcuisinetype: "Indian",
 		rmealpreference: "Vegetarian",
 		rmealtype: "Desserts",
@@ -222,7 +216,19 @@ recipesCollection.insert(  //earlier it was tasksCollection
   
   
        },
-      
-	
+       {
+		rname: "Lime Chicken",
+            rdescription: "In a large mixing bowl or large sealable, Ziplock bag, combine the chicken breasts, freshly squeezed lime juice, ground cumin, sea salt, ground black pepper, cayenne, and fresh cilantro. Toss to combine and coat the chicken well. Cover with plastic wrap (or close Ziplock bag tightly) and place in the fridge for at least 30 minutes, preferably 2-3 hours, to allow the marinade to work. Heat a skillet over medium-high heat with the olive oil. Remove the chicken from the fridge and place in the hot oil. Cook the chicken until well-browned on both sides and cooked through, about 4-6 minutes per side, or until the chicken reaches 165 degrees F internal temperature. Allow to rest for 5 minute before slicing and serving.",
+            rrecipeId: 6,
+		rcuisinetype: "Chinese",
+		rmealpreference: "Non-Vegetarian",
+		rmealtype: "Lunch",
+		rduration: 130,
+            ringredients: "4 boneless skinless chicken breasts, 2 limes approx. 4 tbsp. juice, juiced 1 tsp ground cumin, 1/2 tsp sea salt, 1/2 tsp ground black pepper, 1/8 tsp cayenne pepper, 1/2 cup chopped fresh cilantro*, 1 tbsp olive oil",
+            rchefid: "Div01",
+            rimage:"https://www.simplyrecipes.com/wp-content/uploads/2013/07/grilled-cilantro-lime-chicken-vertical-a-1200.jpg"
+       },
+	]
+}
 )
 
