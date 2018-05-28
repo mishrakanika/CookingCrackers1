77834d46baee9c57d7d4ede14a93fc2b05d84988
Rutu Bhatt <rutubhatt80@gmail.com>
2018-05-28 12:00:12 -0700

    one test is running fine

--- RecipeExpressServer/RecipeAngular/Test/README.md
+++ RecipeExpressServer/RecipeAngular/Test/README.md
@@ -1,6 +1,10 @@
 Go to Test folder
 install mocha using 'npm install -g mocha' using sudo if required
-To run the test1.js type - mocha test1.js in the terminal
+To run the test.js type the following in the terminal - 
+mocha test.js 
+OR
+npm test
+alongwith 'ng serve' on a different terminal.
 
 Install chai before running test to avoid errors related to chai:
 npm install --save-dev chai
--- RecipeExpressServer/RecipeAngular/Test/test.js
+++ RecipeExpressServer/RecipeAngular/Test/test.js
@@ -14,6 +14,8 @@ describe('Test Cooking Crackers result', function () {
 
 	var requestResult;
 	var response;
+	var expect  = require('chai').expect;
+	var request = require('request');
 		 
     // before(function (done) {
     //     chai.request("http://localhost:8080")
@@ -27,19 +29,27 @@ describe('Test Cooking Crackers result', function () {
 	// 		});
     //     });
     
-    it('Should return an array object with more than 1 object', function (){
-		expect(response).to.have.status(200);
-//        expect(response.body).to.be.an.object;
-		expect(response.body).to.have.length.above(2);
-		expect(response).to.have.headers;
-    });
+//     it('Recipe list should not be empty', function (){
+// 		expect(response).to.have.status(200);
+// //        expect(response.body).to.be.an.object;
+// 		expect(response.body).to.have.length.above(2);
+// 		expect(response).to.have.headers;
+// 		//assert.equal(9, 3*3);
+//     });
     
-	// it('The first entry in the array has known properties', function(){
-	//     expect(requestResult[0]).to.include.keys('name');
-	//     expect(requestResult[0]).to.have.property('_id');
-	// 	expect(response.body[0]).to.have.deep.property('list');
-	// 	expect(response.body).to.not.be.a.string;
-	// });
+// 	it('The first entry in the array has known properties', function(){
+// 	    //expect(requestResult[0]).to.include.keys('rname');
+// 	    expect(requestResult[0]).to.have.property('rrecipeId');
+// 		expect(response.body[0]).to.have.deep.property('rdescription');
+// 		//expect(response.body).to.not.be.a.string;
+// 	});
+	//working fine
+	it('Page should contain a known property', function(done) {
+		request('http://localhost:4200' , function(error, response, body) {
+			expect(body).to.contain('name');
+			done();
+		});
+	});
 	// it('The elements in the array have the expecte properties', function(){
 	// 	expect(response.body).to.satisfy(
 	// 		function (body) {
