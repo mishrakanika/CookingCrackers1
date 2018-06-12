a0825131309bac470a5b3db732309dd79de1b47a
divyanshiy12 <yadavd@seattleu.edu>
2018-06-12 00:59:52 -0700

    home page changes

--- RecipeExpressServer/RecipeAngular/src/app/all-recipe/all-recipe.component.ts
+++ RecipeExpressServer/RecipeAngular/src/app/all-recipe/all-recipe.component.ts
@@ -30,28 +30,14 @@ export class AllRecipeComponent implements OnInit {
         private location: Location,
         private recipeService$: RecipeServiceService)
          { 	
-          // console.log("route Details:" + route.snapshot.params.Id);
-          // this.rrecipeId = route.snapshot.params['Id'];
-         //  console.log("Recipe ID :"+ this.rrecipeId);
+         
            recipeService$.retrieveAllRecipe()
            .subscribe(
              result=>{this.recipelist =result},
               () => { },
           () => { } 
            );
-            //  result => { this.rname = result.rname;
-            //   this.rrecipeId = result.rrecipeId;
-            //   this.rmethod = result.rmethod;
-            //   this.rdescription = result.rdescription;
-            //   this.rduration = result.rduration;
-            //   this.ringredients = result.ringredients;
-            //   this.rchefid = result.rchefid;
-            //   this.rcuisinetype = result.rcuisinetype;
-            //   this.rmealpreference = result.rmealpreference;
-            //   this.rmealtype = result.rmealtype;
-            //   this.rimage = result.rimage;
-    
-          //    },
+           
          
     
     
--- RecipeExpressServer/RecipeAngular/src/app/home/home.component.ts
+++ RecipeExpressServer/RecipeAngular/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import IRecipeModel from '../share/IRecipeModel';
+import {RecipeServiceService} from '../recipe-service.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  recipelist: IRecipeModel[];
+        rname: String;
+       rrecipeId: string;
+        rmethod: String;
+        rdescription: String;
+        rcuisinetype: String;
+        rmealpreference: String;
+        rmealtype: String;
+        rduration: Number;
+        ringredients: String;
+        rchefid: String;
+        rimage: String;
+       router$: ActivatedRoute;
+
+  constructor(private route: ActivatedRoute,
+    private location: Location,
+    private recipeService$: RecipeServiceService) {
+
+      recipeService$.retrieveAllRecipe()
+      .subscribe(
+        result=>{this.recipelist =result},
+         () => { },
+     () => { } 
+      );
+
+     }
 
   ngOnInit() {
   }
